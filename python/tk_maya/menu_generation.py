2bd623b1bfc312888a359a1392f967d2868ec12f
manne ohrstrom <manne@shotgunsoftware.com>
2013-01-24 01:23:48 +0000

    Removed context naming logic to be replaced by context str() method.

--- python/tk_maya/menu_generation.py
+++ python/tk_maya/menu_generation.py
@@ -95,26 +95,7 @@ class MenuGenerator(object):
         """        
         
         ctx = self._engine.context
-        
-        if ctx.entity is None:
-            # project-only!
-            ctx_name = "%s" % ctx.project["name"]
-        
-        elif ctx.step is None and ctx.task is None:
-            # entity only
-            # e.g. [Shot ABC_123]
-            ctx_name = "%s %s" % (ctx.entity["type"], ctx.entity["name"])
-
-        else:
-            # we have either step or task
-            task_step = None
-            if ctx.step:
-                task_step = ctx.step.get("name")
-            if ctx.task:
-                task_step = ctx.task.get("name")
-            
-            # e.g. [Lighting, Shot ABC_123]
-            ctx_name = "%s, %s %s" % (task_step, ctx.entity["type"], ctx.entity["name"])
+        ctx_name = str(ctx)        
         
         # create the menu object
         ctx_menu = pm.subMenuItem(label=ctx_name, parent=self._menu_handle)
