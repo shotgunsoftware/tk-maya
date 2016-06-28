92cd8e3b8bb8f2d0831d45b5215ac37eef9f6e86
Manne Öhrström <manne@shotgunsoftware.com>
2016-06-28 11:54:51 +0100

    Removed site context check.

--- engine.py
+++ engine.py
@@ -297,11 +297,6 @@ class MayaEngine(tank.platform.Engine):
         except:
             # ignore all errors. ex: using a core that doesn't support metrics
             pass
-        
-        if self.context.project is None:
-            # must have at least a project in the context to even start!
-            raise tank.TankError("The engine needs at least a project in the context "
-                                 "in order to start! Your context: %s" % self.context)
 
         # Set the Maya project based on config
         self._set_project()
