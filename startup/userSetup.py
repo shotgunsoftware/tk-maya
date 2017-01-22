76c80ab3b35e08527ae724333635c459fb720a2f
Manne Öhrström <manne@shotgunsoftware.com>
2017-01-22 13:17:41 +0000

    Adjustments to logging output at launch

--- startup/userSetup.py
+++ startup/userSetup.py
@@ -23,14 +23,10 @@ def start_toolkit_classic():
     serialized Context to use to startup Toolkit and
     the tk-maya engine and environment.
     """
-    # Verify sgtk can be loaded.
-    try:
-        import sgtk
-    except Exception, e:
-        OpenMaya.MGlobal.displayError(
-            "Shotgun: Could not import sgtk! Disabling for now: %s" % e
-        )
-        return
+    import sgtk
+    logger = sgtk.LogManager.get_logger(__name__)
+
+    logger.debug("Launching toolkit in classic mode.")
 
     # Get the name of the engine to start from the environement
     env_engine = os.environ.get("SGTK_ENGINE")
@@ -59,6 +55,7 @@ def start_toolkit_classic():
 
     try:
         # Start up the toolkit engine from the environment data
+        logger.debug("Launching engine instance '%s' for context %s" % (env_engine, env_context))
         engine = sgtk.platform.start_engine(env_engine, context.sgtk, context)
     except Exception, e:
         OpenMaya.MGlobal.displayError(
@@ -72,6 +69,11 @@ def start_toolkit_with_plugins():
     Parse environment variables for a list of plugins to load that will
     ultimately startup Toolkit and the tk-maya engine and environment.
     """
+    import sgtk
+    logger = sgtk.LogManager.get_logger(__name__)
+
+    logger.debug("Launching maya in plugin mode")
+
     for plugin_path in os.environ["SGTK_LOAD_MAYA_PLUGINS"].split(os.pathsep):
         # Find the appropriate "plugin" sub directory. Maya will not be
         # able to find any plugins under the base directory without this.
@@ -82,12 +84,6 @@ def start_toolkit_with_plugins():
         else:
             load_path = plugin_path
 
-        # Display a message indicating what path plugins are
-        # being loaded from.
-        OpenMaya.MGlobal.displayInfo(
-            "Shotgun: Loading plugins from '%s'" % load_path
-        )
-
         # Load the plugins from the resolved path individually, as the
         # loadPlugin Maya command has difficulties loading all (*) plugins
         # from a path that contains a string in the form of 'v#.#.#':
@@ -95,22 +91,22 @@ def start_toolkit_with_plugins():
         #   // Error: line 1: Plug-in, "/shotgun/site/project/install/app_store/tk-maya/v0.7.10/plugins/basic/plug-ins/*", was not found on MAYA_PLUG_IN_PATH. //
         #   loadPlugin "/shotgun/site/project/install/app_store/tk-maya-no_version/plugins/basic/plug-ins/*";
         #   // Result: shotgun //
-        for plugin in os.listdir(load_path):
-            if not plugin.endswith(".py"):
+        for plugin_filename in os.listdir(load_path):
+            if not plugin_filename.endswith(".py"):
                 # Skip files/directories that are not plugins
                 continue
 
             # Construct the OS agnostic full path to the plugin
             # and attempt to load the plugin. Note that the loadPlugin
             # command always returns a list, even when loading a single plugin.
-            load_plugin = os.path.join(load_path, plugin)
-            OpenMaya.MGlobal.displayInfo(
-                "Shotgun: Attempting to load plugin: %s" % load_plugin
-            )
-            loaded_plugins = cmds.loadPlugin(load_plugin)
+            full_plugin_path = os.path.join(load_path, plugin_filename)
+            logger.debug("Loading plugin %s" % full_plugin_path)
+
+            loaded_plugins = cmds.loadPlugin(full_plugin_path)
+            # note: loadPlugin returns a list of the loaded plugins
             if not loaded_plugins:
                 OpenMaya.MGlobal.displayWarning(
-                    "Shotgun: Could not load plugin: %s" % load_plugin
+                    "Shotgun: Could not load plugin: %s" % full_plugin_path
                 )
                 continue
 
@@ -120,27 +116,31 @@ def start_toolkit():
     Import Toolkit and start up a tk-maya engine based on
     environment variables.
     """
-    OpenMaya.MGlobal.displayInfo(
-        "Shotgun: Starting up Toolkit"
-    )
+
+    # Verify sgtk can be loaded.
+    try:
+        import sgtk
+    except Exception, e:
+        OpenMaya.MGlobal.displayError(
+            "Shotgun: Could not import sgtk! Disabling for now: %s" % e
+        )
+        return
+
+    # start up toolkit logging to file
+    sgtk.LogManager().initialize_base_file_handler("tk-maya")
+
     if os.environ.get("SGTK_LOAD_MAYA_PLUGINS"):
         # Plugins will take care of initalizing everything
-        OpenMaya.MGlobal.displayInfo(
-            "Shotgun: Loading Toolkit plugins ..."
-        )
         start_toolkit_with_plugins()
     else:
         # Rely on the classic boostrapping method
-        OpenMaya.MGlobal.displayInfo(
-            "Shotgun: Bootstrapping Toolkit from environmment variables ..."
-        )
         start_toolkit_classic()
 
     # Check if a file was specified to open and open it.
     file_to_open = os.environ.get("SGTK_FILE_TO_OPEN")
     if file_to_open:
         OpenMaya.MGlobal.displayInfo(
-            "Shotgun: Opening '%s' ..." % file_to_open
+            "Shotgun: Opening '%s'..." % file_to_open
         )
         cmds.file(file_to_open, force=True, open=True)
 
