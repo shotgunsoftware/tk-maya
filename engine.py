6e69e7ed0583c03d5be996f285b07f5a0ffd29a9
fpainchaud <fpainchaud.spiria@gmail.com>
2017-01-27 12:24:22 -0500

    #40474: Zero config Maya engine to use new logging. (#39)
    * Upgrade to use core v0.18 engine logging.
    * Properly report plugin debug messages.
    * Add some plugin bootstrapping debug messages.

--- engine.py
+++ engine.py
@@ -216,10 +216,6 @@ class MayaEngine(tank.platform.Engine):
     Toolkit engine for Maya.
     """
 
-    # Time stamp of the last debug message logged.
-    # Initialize it with the current time for lack of a better value.
-    _debug_msg_time_stamp = time.time()
-
     @property
     def context_change_allowed(self):
         """
@@ -534,13 +530,10 @@ class MayaEngine(tank.platform.Engine):
     ##########################################################################################
     # logging
 
-    def _emit_log_message_FUTURE(self, handler, record):
+    def _emit_log_message(self, handler, record):
         """
         Called by the engine to log messages in Maya script editor.
         All log messages from the toolkit logging namespace will be passed to this method.
-
-        .. note:: This method will be called by the new logging system introduced in core v0.18.
-                  It will be enabled in January 2017 by removing the method name suffix "_FUTURE".
 
         :param handler: Log handler that this message was dispatched from.
                         Its default format is "[levelname basename] message".
@@ -550,12 +543,13 @@ class MayaEngine(tank.platform.Engine):
         """
 
         # Give a standard format to the message:
-        #     Shotgun DEBUG <basename>: <message>
-        # or
         #     Shotgun <basename>: <message>
-        msg = "Shotgun %s%s: %s" % ("DEBUG " if record.levelno < logging.INFO else "",
-                                    record.basename,
-                                    record.message)
+        # where "basename" is the leaf part of the logging record name,
+        # for example "tk-multi-shotgunpanel" or "qt_importer".
+        if record.levelno < logging.INFO:
+            msg = "Debug: Shotgun %s: %s" % (record.basename, record.message)
+        else:
+            msg = "Shotgun %s: %s" % (record.basename, record.message)
 
         # Select Maya display function to use according to the logging record level.
         if record.levelno < logging.WARNING:
@@ -567,95 +561,6 @@ class MayaEngine(tank.platform.Engine):
 
         # Display the message in Maya script editor in a thread safe manner.
         self.async_execute_in_main_thread(fct, msg)
-
-    def log_debug(self, msg):
-        """
-        Logs debug messages in Maya script editor.
-
-        .. note:: This method is called by the legacy logging system.
-                  The new logging method will take over in January 2017
-                  while preserving backward compatibility.
-
-        :param msg: Debug message to log.
-                    This message usually has the format "basename: text" where
-                    "basename" is the leaf part of the logging record name,
-                    for example "tk-multi-shotgunpanel" or "qt_importer".
-        """
-
-        # Do not log debug messages when debug logging setting is off.
-        if not self.get_setting("debug_logging", False):
-            return
-
-        current_time_stamp = time.time()
-
-        # Give a standard format to the message.
-        msg = "Shotgun DEBUG (%0.3fs): %s" % (current_time_stamp-self._debug_msg_time_stamp, msg)
-
-        # Display the message in Maya script editor in a thread safe manner.
-        self.async_execute_in_main_thread(OpenMaya.MGlobal.displayInfo, msg)
-
-        # Update the debug message time stamp.
-        self._debug_msg_time_stamp = current_time_stamp
-
-    def log_info(self, msg):
-        """
-        Logs info messages in Maya script editor.
-
-        .. note:: This method is called by the legacy logging system.
-                  The new logging method will take over in January 2017
-                  while preserving backward compatibility.
-
-        :param msg: Info message to log.
-                    This message usually has the format "basename: text" where
-                    "basename" is the leaf part of the logging record name,
-                    for example "tk-multi-shotgunpanel" or "qt_importer".
-        """
-
-        # Give a standard format to the message.
-        msg = "Shotgun: %s" % msg
-
-        # Display the message in Maya script editor in a thread safe manner.
-        self.async_execute_in_main_thread(OpenMaya.MGlobal.displayInfo, msg)
-
-    def log_warning(self, msg):
-        """
-        Logs warning messages in Maya script editor.
-
-        .. note:: This method is called by the legacy logging system.
-                  The new logging method will take over in January 2017
-                  while preserving backward compatibility.
-
-        :param msg: Warning message to log.
-                    This message usually has the format "basename: text" where
-                    "basename" is the leaf part of the logging record name,
-                    for example "tk-multi-shotgunpanel" or "qt_importer".
-        """
-
-        # Give a standard format to the message.
-        msg = "Shotgun: %s" % msg
-
-        # Display the message in Maya script editor in a thread safe manner.
-        self.async_execute_in_main_thread(OpenMaya.MGlobal.displayWarning, msg)
-
-    def log_error(self, msg):
-        """
-        Logs error messages in Maya script editor.
-
-        .. note:: This method is called by the legacy logging system.
-                  The new logging method will take over in January 2017
-                  while preserving backward compatibility.
-
-        :param msg: Error message to log.
-                    This message usually has the format "basename: text" where
-                    "basename" is the leaf part of the logging record name,
-                    for example "tk-multi-shotgunpanel" or "qt_importer".
-        """
-
-        # Give a standard format to the message.
-        msg = "Shotgun: %s" % msg
-
-        # Display the message in Maya script editor in a thread safe manner.
-        self.async_execute_in_main_thread(OpenMaya.MGlobal.displayError, msg)
 
     ##########################################################################################
     # scene and project management
--- plugins/basic/python/tk_maya_basic/plugin_logging.py
+++ plugins/basic/python/tk_maya_basic/plugin_logging.py
@@ -10,7 +10,7 @@
 
 import logging
 
-import maya.api.OpenMaya as OpenMaya2  # Python API 2.0
+import maya.mel as mel
 import maya.utils
 
 
@@ -22,16 +22,13 @@ class PluginLoggingHandler(logging.Handler):
     def __init__(self):
         """
         Initializes an instance of the plug-in logging handler.
-
-        :param plugin_name: Plug-in name to include in the standard message format.
         """
 
         # Avoid using super() in order to be compatible with old-style classes found in older versions of logging.
         logging.Handler.__init__(self)
 
-        # Set the handler to use a standard message format similar to the one used by the engine.
-        logging_format = "Shotgun: %(message)s"
-        self.setFormatter(logging.Formatter(logging_format))
+        # Set the handler to use a simple message format.
+        self.setFormatter(logging.Formatter("%(message)s"))
 
     def emit(self, record):
         """
@@ -41,15 +38,25 @@ class PluginLoggingHandler(logging.Handler):
 
         :param record: Logging record to display in Maya script editor.
         """
-        # Give a standard format to the message.
-        msg = self.format(record)
+
+        # Give a standard format to the message:
+        #     Shotgun: <message>
+        # We use a simpler message format than the one used by the engine.
+        if record.levelno < logging.INFO:
+            msg = "Debug: Shotgun: %s" % self.format(record)
+        else:
+            msg = "Shotgun: %s" % self.format(record)
+
+        # Select Maya display function to use according to the logging record level.
+        # We use MEL to display the message in order to have it surrounded by "//"
+        # rather than "#" to go along with the messages emitted by the engine.
+        if record.levelno < logging.WARNING:
+            mel_fct = 'print "// %s //\\n";' % msg
+        elif record.levelno < logging.ERROR:
+            mel_fct = 'warning -noContext "%s";' % msg
+        else:
+            # We use MEL "catch" to ignore the exception raised by MEL "error".
+            mel_fct = 'catch( `error -noContext "%s"` );' % msg
 
         # Display the message in Maya script editor in a thread safe manner.
-        if record.levelno < logging.WARNING:
-            fct = OpenMaya2.MGlobal.displayInfo
-        elif record.levelno < logging.ERROR:
-            fct = OpenMaya2.MGlobal.displayWarning
-        else:
-            fct = OpenMaya2.MGlobal.displayError
-
-        maya.utils.executeInMainThreadWithResult(fct, msg)
+        maya.utils.executeInMainThreadWithResult(mel.eval, mel_fct)
