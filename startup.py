18037f210450adbead4e8834d9ab5f1f7ed203e7
Josh Tomlinson <josh.tomlinson@shotgunsoftware.com>
2017-03-14 08:46:43 -0400

    Maya startup now uses template/glob pattern from other engines. (#46)

--- startup.py
+++ startup.py
@@ -10,9 +10,6 @@
 
 import os
 import sys
-import re
-import glob
-import subprocess
 
 import sgtk
 from sgtk.platform import SoftwareLauncher, SoftwareVersion, LaunchInformation
@@ -24,6 +21,35 @@ class MayaLauncher(SoftwareLauncher):
     a tk-maya engine with the current context in the new session
     of Maya.
     """
+
+    # Named regex strings to insert into the executable template paths when
+    # matching against supplied versions and products. Similar to the glob
+    # strings, these allow us to alter the regex matching for any of the
+    # variable components of the path in one place
+    COMPONENT_REGEX_LOOKUP = {
+        "version": "[\d.]+"
+    }
+
+    # This dictionary defines a list of executable template strings for each
+    # of the supported operating systems. The templates are used for both
+    # globbing and regex matches by replacing the named format placeholders
+    # with an appropriate glob or regex string. As Side FX adds modifies the
+    # install path on a given OS for a new release, a new template will need
+    # to be added here.
+    EXECUTABLE_TEMPLATES = {
+        "darwin": [
+            # /Applications/Autodesk/maya2015/Maya.app
+            "/Applications/Autodesk/maya{version}/Maya.app",
+        ],
+        "win32": [
+            # C:/Program Files/Autodesk/Maya2015/bin/maya.exe
+            "C:/Program Files/Autodesk/Maya{version}/bin/maya.exe",
+        ],
+        "linux2": [
+            # /usr/autodesk/maya2016/bin/maya
+            "/usr/autodesk/maya{version}/bin/maya",
+        ]
+    }
 
     @property
     def minimum_supported_version(self):
@@ -123,6 +149,10 @@ class MayaLauncher(SoftwareLauncher):
 
         :returns: Full path to application icon as a string or None.
         """
+
+        # the engine icon in case we need to use it as a fallback
+        engine_icon = os.path.join(self.disk_location, "icon_256.png")
+
         self.logger.debug(
             "Looking for Application icon for executable '%s' ..." %
             exec_path
@@ -141,22 +171,19 @@ class MayaLauncher(SoftwareLauncher):
             icon_base_path = "".join(exec_path.partition("bin")[0:1])
 
         if not icon_base_path:
-            # If no base path, no icon
-            self.logger.debug(
-                "Could not resolve icon base path for executable '%s'." %
-                exec_path
-            )
-            return None
+            # use the bundled engine icon
+            self.logger.debug("Couldn't find bundled icon. Using engine icon.")
+            return engine_icon
 
         # Append the standard icon to the base path and
         # return that path if it exists, else None.
         icon_path = os.path.join(icon_base_path, "icons", "mayaico.png")
         if not os.path.exists(icon_path):
             self.logger.debug(
-                "Icon path '%s' resolved from executable '%s' does not exist!" %
-                (icon_path, exec_path)
+                "Icon path '%s' resolved from executable '%s' does not exist!"
+                "Falling back on engine icon." % (icon_path, exec_path)
             )
-            return None
+            return engine_icon
 
         # Record what the resolved icon path was.
         self.logger.debug(
@@ -165,136 +192,62 @@ class MayaLauncher(SoftwareLauncher):
         )
         return icon_path
 
-    def _resolve_path_for_platform(self, exec_path):
-        """
-        In some cases, the resolved path to an executable won't launch properly
-        as is. Identify those cases here and modify the input executable path
-        into something that will launch correctly on the current platform.
-
-        :param str exec_path: Full path to a Maya executable
-
-        :returns: Full path to a Maya executable that will properly launch
-                  on the current platform.
-        """
-        if not exec_path:
-            # Nothing to check
-            return exec_path
-
-        if sys.platform == "darwin" and "Maya.app" in exec_path:
-            # There seems to be an anomoly with launching Maya from the
-            # full executable path on MacOS. Everything behaves nicer if
-            # Maya is launched from the bundle Maya.app directory instead.
-            exec_path = "".join(exec_path.partition("Maya.app")[0:2])
-
-        elif sys.platform == "linux2" and exec_path.endswith("maya.bin"):
-            # Specifying the .bin suffix when launching sometimes results
-            # in linked libraries not getting loaded correctly. Stripping
-            # off the .bin suffix seems to resolve that issue.
-            path_parts = exec_path.split(os.path.sep)
-            path_parts[-1] = path_parts[-1].replace("maya.bin", "maya")
-            exec_path = os.path.sep.join(path_parts)
-
-        return exec_path
-
     def scan_software(self):
         """
-        Creates SoftwareVersion instances based on the path values used
-        in the default configuration paths.yml environment.
+        Scan the filesystem for maya executables.
 
-        :returns: List of :class:`SoftwareVersion` instances
+        :return: A list of :class:`SoftwareVersion` objects.
         """
-        # Determine a list of paths to search for Maya executables based
-        # on default installation path(s) for the current platform
-        search_paths = []
-        exec_paths = []
-        if sys.platform == "darwin":
-            search_paths = glob.glob("/Applications/Autodesk/maya*")
 
-        elif sys.platform == "win32":
-            search_paths = glob.glob("C:\Program Files\Autodesk\Maya*")
+        self.logger.debug("Scanning for Maya executables...")
 
-        elif sys.platform == "linux2":
-            exec_paths.append("maya")
-
-        if search_paths:
-            for search_path in search_paths:
-                # Construct the expected executable name for this path.
-                # If it exists, add it to the list of exec_paths to check.
-                exec_path = None
-                if sys.platform == "darwin":
-                    exec_path = os.path.join(search_path, "Maya.app", "Contents", "bin", "maya")
-
-                elif sys.platform == "win32":
-                    exec_path = os.path.join(search_path, "bin", "maya.exe")
-
-                if exec_path and os.path.exists(exec_path):
-                    exec_paths.append(exec_path)
-
-        sw_versions = []
-
-        default_product = "Maya"
-
-        if exec_paths:
-            for exec_path in exec_paths:
-                # Check to see if the version number can be parsed from the path name.
-                path_sw_versions = [
-                    p.lower() for p in exec_path.split(os.path.sep)
-                    if re.match("maya[0-9]+[.0-9]*$", p.lower()) is not None
-                ]
-                if path_sw_versions:
-                    # Use this sub dir to determine the default product name
-                    # and version for the SoftwareVersion to be created.
-                    exec_dir = path_sw_versions[0]
-                    default_version = exec_dir.replace("maya", "")
-                    self.logger.debug(
-                        "Resolved version '%s' from executable '%s'." %
-                        (default_version, exec_path)
-                    )
-                else:
-                    try:
-                        # This works, but makes the Desktop project window disappear
-                        # for some reason, which isn't very nice. Therefore, use as a
-                        # last resort.
-                        version_output = subprocess.check_output([exec_path, "-v"])
-                    except OSError:
-                        self.logger.exception(
-                            "Could not retrieve version information from default "
-                            "executable path '%s'." % exec_path
-                        )
-                        continue
-
-                    # Display and version information are contained before the first ','
-                    # in the output version string.
-                    default_product = version_output[0:version_output.find(",")]
-
-                    # Update known oddball display values to "nicer" version numbers.
-                    if "2016 Extension 2 SP1" in default_product:
-                        default_product = default_product.replace("2016 Extension 2 SP1", "2016.5")
-
-                    # Parse the default version from the display name determined from
-                    # the version output.
-                    default_version = default_product.lower().replace("maya", "").strip()
-                    self.logger.debug(
-                        "Resolved version '%s' from version output '%s'" %
-                        (default_version, version_output)
-                    )
-
-                # Create a SoftwareVersion using the information from executable
-                # path(s) found in default locations.
-                exec_path = self._resolve_path_for_platform(exec_path)
-                self.logger.debug("Creating SoftwareVersion for executable '%s'." % exec_path)
-
-                sw = SoftwareVersion(
-                    default_version,
-                    default_product,
-                    exec_path,
-                    self._icon_from_executable(exec_path)
+        supported_sw_versions = []
+        for sw_version in self._find_software():
+            (supported, reason) = self._is_supported(sw_version)
+            if supported:
+                supported_sw_versions.append(sw_version)
+            else:
+                self.logger.debug(
+                    "SoftwareVersion %s is not supported: %s" %
+                    (sw_version, reason)
                 )
 
-                supported, reason = self._is_supported(sw)
-                if supported:
-                    sw_versions.append(sw)
-                else:
-                    self.logger.debug(reason)
+        return supported_sw_versions
+
+    def _find_software(self):
+        """
+        Find executables in the default install locations.
+        """
+
+        # all the executable templates for the current OS
+        executable_templates = self.EXECUTABLE_TEMPLATES.get(sys.platform, [])
+
+        # all the discovered executables
+        sw_versions = []
+
+        for executable_template in executable_templates:
+
+            self.logger.debug("Processing template %s.", executable_template)
+
+            executable_matches = self._glob_and_match(
+                executable_template,
+                self.COMPONENT_REGEX_LOOKUP
+            )
+
+            # Extract all products from that executable.
+            for (executable_path, key_dict) in executable_matches:
+
+                # extract the matched keys form the key_dict (default to None if
+                # not included)
+                executable_version = key_dict.get("version")
+
+                sw_versions.append(
+                    SoftwareVersion(
+                        executable_version,
+                        "Maya",
+                        executable_path,
+                        self._icon_from_executable(executable_path)
+                    )
+                )
 
         return sw_versions
