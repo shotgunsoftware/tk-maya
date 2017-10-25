c8eb779caab0bd5e715d3a3543c88978b6c29e16
Josh Tomlinson <josh.tomlinson@shotgunsoftware.com>
2017-10-25 10:25:04 -0400

    WIP: Updates session geo plugin to use new inheritance scheme. Adds a more consistent icon.

--- hooks/tk-multi-publish2/basic/publish_session_geometry.py
+++ hooks/tk-multi-publish2/basic/publish_session_geometry.py
@@ -13,8 +13,6 @@ import pprint
 import maya.cmds as cmds
 import maya.mel as mel
 import sgtk
-from sgtk.util.filesystem import copy_file
-import traceback
 
 HookBaseClass = sgtk.get_hook_baseclass()
 
@@ -22,28 +20,16 @@ HookBaseClass = sgtk.get_hook_baseclass()
 class MayaSessionGeometryPublishPlugin(HookBaseClass):
     """
     Plugin for publishing an open maya session.
+
+    This hook relies on functionality found in the base file publisher hook in
+    the publish2 app and should inherit from it in the configuration. The hook
+    setting for this plugin should look something like this::
+
+        hook: "{self}/publish_file.py:{engine}/tk-multi-publish2/basic/publish_session.py"
+
     """
 
-    @property
-    def icon(self):
-        """
-        Path to an png icon on disk
-        """
-
-        # look for icon one level up from this hook's folder in "icons" folder
-        return os.path.join(
-            self.disk_location,
-            os.pardir,
-            "icons",
-            "publish.png"
-        )
-
-    @property
-    def name(self):
-        """
-        One line display name describing the plugin
-        """
-        return "Publish to Shotgun"
+    # NOTE: The plugin icon and name are defined by the base file plugin.
 
     @property
     def description(self):
@@ -52,46 +38,12 @@ class MayaSessionGeometryPublishPlugin(HookBaseClass):
         contain simple html for formatting.
         """
 
-        loader_url = "https://support.shotgunsoftware.com/hc/en-us/articles/219033078"
-
         return """
-        Publishes the session's geometry to Shotgun. A <b>Publish</b> entry will be
-        created in Shotgun which will include a reference to the file's current
-        path on disk. If templates are configured and the work file template
-        matches the current session path, the work file will be copied to the
-        publish file template path which will be the file that is published.
-        Other users will be able to access the published file via the
-        <b><a href='%s'>Loader</a></b> so long as they have access to the file's
-        location on disk.
-
-        If the session has no geometry nothing will be exported or published.
-
-        <h3>File versioning</h3>
-        The version number, will be derived from the session's project file
-        version number and will be incremented in sync with it.
-
-        The <code>version</code> field of the resulting <b>Publish</b> in
-        Shotgun will also reflect the version number identified in the filename.
-        The basic worklfow recognizes the following version formats by default:
-
-        <ul>
-        <li><code>filename.v###.ext</code></li>
-        <li><code>filename_v###.ext</code></li>
-        <li><code>filename-v###.ext</code></li>
-        </ul>
-
-        If templates are configured, the version will be determined from the
-        "{version}" token.
-
-        <br><br><i>NOTE: any amount of version number padding is supported. for
-        non-template based workflows.</i>
-
-        <h3>Overwriting an existing publish</h3>
-        In non-template workflows, a file can be published multiple times,
-        however only the most recent publish will be available to other users.
-        Warnings will be provided during validation if there are previous
-        publishes.
-        """ % (loader_url,)
+        This plugin publishes session geometry for the current session. Any
+        session geometry will be exported to the path defined by this plugin's
+        configured "Publish Template" setting. The plugin will fail to validate
+        if the "AbcExport" plugin is not enabled or cannot be found.
+        """
 
     @property
     def settings(self):
@@ -112,20 +64,24 @@ class MayaSessionGeometryPublishPlugin(HookBaseClass):
         The type string should be one of the data types that toolkit accepts as
         part of its environment configuration.
         """
-        return {
-            "Publish Type": {
-                "type": "shotgun_publish_type",
-                "default": "Alembic Cache",
-                "description": "SG publish type to associate publishes with."
-            },
+        # inherit the settings from the base publish plugin
+        base_settings = super(MayaSessionGeometryPublishPlugin, self).settings or {}
+
+        # settings specific to this class
+        maya_publish_settings = {
             "Publish Template": {
                 "type": "template",
                 "default": None,
                 "description": "Template path for published work files. Should"
                                "correspond to a template defined in "
-                               "templates.yml."
+                               "templates.yml.",
             }
         }
+
+        # update the base settings
+        base_settings.update(maya_publish_settings)
+
+        return base_settings
 
     @property
     def item_filters(self):
@@ -168,16 +124,36 @@ class MayaSessionGeometryPublishPlugin(HookBaseClass):
         publisher = self.parent
         template_name = settings["Publish Template"].value
 
-        publish_template = publisher.get_template_by_name(template_name)
-        item.properties["publish_template"] = publish_template
-        if not publish_template and not item.parent.properties.get("work_template"):
+        # ensure a work file template is available on the parent item
+        work_template = item.parent.properties.get("work_template")
+        if not work_template:
+            self.logger.debug(
+                "A work template is required for the session item in order to "
+                "publish session geometry. Not accepting session geom item."
+            )
             accepted = False
+
+        # ensure the publish template is defined and valid and that we also have
+        publish_template = publisher.get_template_by_name(template_name)
+        if not publish_template:
+            self.logger.debug(
+                "The valid publish template could not be determined for the "
+                "session geometry item. Not accepting the item."
+            )
+            accepted = False
+
+        # we've validated the publish template. add it to the item properties
+        # for use in subsequent methods
+        item.properties["publish_template"] = publish_template
 
         # check that the AbcExport command is available!
         if not mel.eval("exists \"AbcExport\""):
+            self.logger.debug(
+                "Item not accepted because alembic export command 'AbcExport' "
+                "is not available. Perhaps the plugin is not enabled?"
+            )
             accepted = False
-            self.logger.warn("Item not accepted because export command AbcExport is not available.")
-        
+
         return {
             "accepted": accepted,
             "checked": True
@@ -197,7 +173,10 @@ class MayaSessionGeometryPublishPlugin(HookBaseClass):
 
         # check that there is still geometry in the scene:
         if not cmds.ls(geometry=True, noIntermediate=True):
-            self.logger.warn("Validation failed because there is no geometry in the scene to be exported")
+            self.logger.warning(
+                "Validation failed because there is no geometry in the scene "
+                "to be exported."
+            )
             return False
 
         publisher = self.parent
@@ -206,66 +185,34 @@ class MayaSessionGeometryPublishPlugin(HookBaseClass):
         work_template = item.parent.properties.get("work_template")
         publish_template = item.properties.get("publish_template")
 
-        # get the current scene path and extract fields from it
-        # using the work template:
-        scene_path = sgtk.util.ShotgunPath.normalize(self._session_path())
-        #scene_file_name = os.path.basename(scene_path)
-        scene_file_name = publisher.util.get_publish_name(scene_path)
-        scene_file_name_root, scene_file_name_ext = os.path.splitext(scene_file_name)
-        fields = work_template.get_fields(scene_path)
+        # get the current scene path and extract fields from it using the work
+        # template:
+        scene_path = sgtk.util.ShotgunPath.normalize(_session_path())
+        work_fields = work_template.get_fields(scene_path)
+
+        # ensure the fields work for the publish template
+        missing_keys = publish_template.missing_keys(work_fields)
+        if missing_keys:
+            self.logger.error(
+                "Work file '%s' missing keys required for the publish "
+                "template: %s" % (scene_path, missing_keys)
+            )
+            return False
                 
-        # create the publish path by applying the fields 
-        # with the publish template:
-        publish_path = publish_template.apply_fields(fields)
-        
-        # determine the publish path, version, type, and name
-        publish_name = publisher.util.get_publish_name(publish_path)
+        # create the publish path by applying the fields. store it in the item's
+        # properties. This is the path we'll create and then publish in the base
+        # publish plugin. Also set the publish_path to be explicit.
+        item.properties["path"] = publish_template.apply_fields(work_fields)
+        item.properties["publish_path"] = item.properties["path"]
 
-        # get all the publish info extracted during validation
-        version_number = publisher.util.get_version_number(publish_path) or 1
-        publish_type = settings["Publish Type"].value
 
-        publish_info = { "path": publish_path,
-                         "name": publish_name,
-                         "version": version_number,
-                         "type": publish_type,
-                       }
+        # use the work file's version number when publishing
+        if "version" in work_fields:
+            item.properties["publish_version"] = work_fields["version"]
 
-        item.properties["publish_info"] = publish_info
-
-        # see if there are any other publishes of this path with a status.
-        # Note the name, context, and path *must* match the values supplied to
-        # register_publish in the publish phase in order for this to return an
-        # accurate list of previous publishes of this file.
-        publishes = publisher.util.get_conflicting_publishes(
-            item.context,
-            publish_path,
-            publish_name,
-            filters=["sg_status_list", "is_not", None]
-        )
-
-        if publishes:
-            conflict_info = (
-                "If you continue, these conflicting publishes will no longer "
-                "be available to other users via the loader:<br>"
-                "<pre>%s</pre>" % (pprint.pformat(publishes),)
-            )
-            self.logger.warn(
-                "Found %s conflicting publishes in Shotgun" %
-                (len(publishes),),
-                extra={
-                    "action_show_more_info": {
-                        "label": "Show Conflicts",
-                        "tooltip": "Show the conflicting publishes in Shotgun",
-                        "text": conflict_info
-                    }
-                }
-            )
-
-        self.logger.info("A Publish will be created in Shotgun and linked to:")
-        self.logger.info("  %s" % (publish_path,))
-
-        return True
+        # run the base class validation
+        return super(MayaSessionGeometryPublishPlugin, self).validate(
+            settings, item)
 
     def publish(self, settings, item):
         """
@@ -279,30 +226,28 @@ class MayaSessionGeometryPublishPlugin(HookBaseClass):
 
         publisher = self.parent
 
-        publish_info = item.properties["publish_info"]
-
-        # get the work file path stored during validation
-        publish_path = publish_info["path"]
-
-        # get all the publish info extracted during validation
-        version_number = publish_info["version"]
-        publish_name = publish_info["name"]
-        publish_type = publish_info["type"]
+        # get the path to create and publish
+        publish_path = item.properties["path"]
 
         # ensure the publish folder exists:
         publish_folder = os.path.dirname(publish_path)
         self.parent.ensure_folder_exists(publish_folder)
 
-        # set the alembic args that make the most sense when working with Mari.  These flags
-        # will ensure the export of an Alembic file that contains all visible geometry from
-        # the current scene together with UV's and face sets for use in Mari.
-        alembic_args = ["-renderableOnly",   # only renderable objects (visible and not templated)
-                        "-writeFaceSets",    # write shading group set assignments (Maya 2015+)
-                        "-uvWrite"           # write uv's (only the current uv set gets written)
-                        ]        
+        # set the alembic args that make the most sense when working with Mari.
+        # These flags will ensure the export of an Alembic file that contains
+        # all visible geometry from the current scene together with UV's and
+        # face sets for use in Mari.
+        alembic_args = [
+            # only renderable objects (visible and not templated)
+            "-renderableOnly",
+            # write shading group set assignments (Maya 2015+)
+            "-writeFaceSets",
+            # write uv's (only the current uv set gets written)
+            "-uvWrite"
+        ]
 
         # find the animated frame range to use:
-        start_frame, end_frame = self._find_scene_animation_range()
+        start_frame, end_frame = _find_scene_animation_range()
         if start_frame and end_frame:
             alembic_args.append("-fr %d %d" % (start_frame, end_frame))
 
@@ -322,108 +267,40 @@ class MayaSessionGeometryPublishPlugin(HookBaseClass):
             self.logger.error("Failed to export Geometry: %s" % e)
             return
 
-        # arguments for publish registration
-        self.logger.info("Registering publish...")
-        publish_data = {
-            "tk": publisher.sgtk,
-            "context": item.context,
-            "comment": item.description,
-            "path": publish_path,
-            "name": publish_name,
-            "version_number": version_number,
-            "thumbnail_path": item.get_thumbnail_as_path(),
-            "published_file_type": publish_type,
-            "dependency_paths": [],
-        }
+        # Now that the path has been generated, hand it off to the
+        super(MayaSessionGeometryPublishPlugin, self).publish(settings, item)
 
-        # log the publish data for debugging
-        self.logger.debug(
-            "Populated Publish data...",
-            extra={
-                "action_show_more_info": {
-                    "label": "Publish Data",
-                    "tooltip": "Show the complete Publish data dictionary",
-                    "text": "<pre>%s</pre>" % (pprint.pformat(publish_data),)
-                }
-            }
-        )
 
-        # create the publish and stash it in the item properties for other
-        # plugins to use.
-        item.properties["sg_publish_data"] = sgtk.util.register_publish(
-            **publish_data)
+def _find_scene_animation_range():
+    """
+    Find the animation range from the current scene.
+    """
+    # look for any animation in the scene:
+    animation_curves = cmds.ls(typ="animCurve")
 
-        # inject the publish path such that children can refer to it when
-        # updating dependency information
-        item.properties["sg_publish_path"] = publish_path
+    # if there aren't any animation curves then just return
+    # a single frame:
+    if not animation_curves:
+        return 1, 1
 
-        self.logger.info("Publish registered!")
+    # something in the scene is animated so return the
+    # current timeline.  This could be extended if needed
+    # to calculate the frame range of the animated curves.
+    start = int(cmds.playbackOptions(q=True, min=True))
+    end = int(cmds.playbackOptions(q=True, max=True))
 
-    def finalize(self, settings, item):
-        """
-        Execute the finalization pass. This pass executes once all the publish
-        tasks have completed, and can for example be used to version up files.
+    return start, end
 
-        :param settings: Dictionary of Settings. The keys are strings, matching
-            the keys returned in the settings property. The values are `Setting`
-            instances.
-        :param item: Item to process
-        """
 
-        publisher = self.parent
+def _session_path():
+    """
+    Return the path to the current session
+    :return:
+    """
+    path = cmds.file(query=True, sn=True)
 
-        # get the data for the publish that was just created in SG
-        publish_data = item.properties["sg_publish_data"]
+    if isinstance(path, unicode):
+        path = path.encode("utf-8")
 
-        # ensure conflicting publishes have their status cleared
-        publisher.util.clear_status_for_conflicting_publishes(
-            item.context, publish_data)
-
-        self.logger.info(
-            "Cleared the status of all previous, conflicting publishes")
-
-        publish_path = item.properties["publish_info"]["path"]
-
-        self.logger.info(
-            "Publish created for file: %s" % (publish_path,),
-            extra={
-                "action_show_in_shotgun": {
-                    "label": "Show Publish",
-                    "tooltip": "Open the Publish in Shotgun.",
-                    "entity": publish_data
-                }
-            }
-        )
-
-    def _find_scene_animation_range(self):
-        """
-        Find the animation range from the current scene.
-        """
-        # look for any animation in the scene:
-        animation_curves = cmds.ls(typ="animCurve")
-        
-        # if there aren't any animation curves then just return
-        # a single frame:
-        if not animation_curves:
-            return (1, 1)
-        
-        # something in the scene is animated so return the
-        # current timeline.  This could be extended if needed
-        # to calculate the frame range of the animated curves.
-        start = int(cmds.playbackOptions(q=True, min=True))
-        end = int(cmds.playbackOptions(q=True, max=True))        
-        
-        return (start, end)
-
-    def _session_path(self):
-        """
-        Return the path to the current session
-        :return:
-        """
-        path = cmds.file(query=True, sn=True)
-
-        if isinstance(path, unicode):
-            path = path.encode("utf-8")
-
-        return path
+    return path
 
