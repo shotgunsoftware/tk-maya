0252f774781c67cf17fbb1470d4afb91b96bff8e
Yungsiow Yang <yungsiow.yang@autodesk.com>
2026-08-12 16:27:12 -0400

    handle bg publish for alembic export
    - defer alembic export and publish registration to bg process if enabled
    - defer finalize action if bg publish is enabled

--- hooks/tk-multi-publish2/basic/publish_session_geometry.py
+++ hooks/tk-multi-publish2/basic/publish_session_geometry.py
@@ -237,51 +237,88 @@ class MayaSessionGeometryPublishPlugin(HookBaseClass):
         :param item: Item to process
         """
 
-        publisher = self.parent
+        if self._is_deferred_to_bg(item):
+            self.logger.info(
+                "Background publish enabled — deferring alembic export and publish to the background process."
+            )
+        else:
+            publisher = self.parent
 
-        # get the path to create and publish
-        publish_path = item.properties["path"]
+            # get the path to create and publish
+            publish_path = item.properties["path"]
 
-        # ensure the publish folder exists:
-        publish_folder = os.path.dirname(publish_path)
-        self.parent.ensure_folder_exists(publish_folder)
+            # ensure the publish folder exists:
+            publish_folder = os.path.dirname(publish_path)
+            self.parent.ensure_folder_exists(publish_folder)
 
-        # set the alembic args that make the most sense when working with Mari.
-        # These flags will ensure the export of an Alembic file that contains
-        # all visible geometry from the current scene together with UV's and
-        # face sets for use in Mari.
-        alembic_args = [
-            # only renderable objects (visible and not templated)
-            "-renderableOnly",
-            # write shading group set assignments (Maya 2015+)
-            "-writeFaceSets",
-            # write uv's (only the current uv set gets written)
-            "-uvWrite",
-        ]
+            # set the alembic args that make the most sense when working with Mari.
+            # These flags will ensure the export of an Alembic file that contains
+            # all visible geometry from the current scene together with UV's and
+            # face sets for use in Mari.
+            alembic_args = [
+                # only renderable objects (visible and not templated)
+                "-renderableOnly",
+                # write shading group set assignments (Maya 2015+)
+                "-writeFaceSets",
+                # write uv's (only the current uv set gets written)
+                "-uvWrite",
+            ]
 
-        # find the animated frame range to use:
-        start_frame, end_frame = _find_scene_animation_range()
-        if start_frame and end_frame:
-            alembic_args.append("-fr %d %d" % (start_frame, end_frame))
+            # find the animated frame range to use:
+            start_frame, end_frame = _find_scene_animation_range()
+            if start_frame and end_frame:
+                alembic_args.append("-fr %d %d" % (start_frame, end_frame))
 
-        # Set the output path:
-        # Note: The AbcExport command expects forward slashes!
-        alembic_args.append("-file '%s'" % publish_path.replace("\\", "/"))
+            # Set the output path:
+            # Note: The AbcExport command expects forward slashes!
+            alembic_args.append("-file '%s'" % publish_path.replace("\\", "/"))
 
-        # build the export command.  Note, use AbcExport -help in Maya for
-        # more detailed Alembic export help
-        abc_export_cmd = 'AbcExport -j "%s"' % " ".join(alembic_args)
+            # build the export command.  Note, use AbcExport -help in Maya for
+            # more detailed Alembic export help
+            abc_export_cmd = 'AbcExport -j "%s"' % " ".join(alembic_args)
 
-        # ...and execute it:
-        try:
-            self.parent.log_debug("Executing command: %s" % abc_export_cmd)
-            mel.eval(abc_export_cmd)
-        except Exception as e:
-            self.logger.error("Failed to export Geometry: %s" % e)
-            return
+            # ...and execute it:
+            try:
+                # Load Alembic plugin if it isn't already
+                if not cmds.pluginInfo("AbcExport", query=True, loaded=True):
+                    cmds.loadPlugin("AbcExport")
+                self.parent.log_debug("Executing command: %s" % abc_export_cmd)
+                mel.eval(abc_export_cmd)
+            except Exception as e:
+                self.logger.error("Failed to export Geometry: %s" % e)
+                return
 
-        # Now that the path has been generated, hand it off to the
-        super().publish(settings, item)
+            # Now that the path has been generated, hand it off to the
+            super().publish(settings, item)
+
+    def finalize(self, settings, item):
+        """
+        Execute the finalization pass. This pass executes once all the publish
+        tasks have completed, and can for example be used to version up files.
+
+        :param settings: Dictionary of Settings. The keys are strings, matching
+            the keys returned in the settings property. The values are `Setting`
+            instances.
+        :param item: Item to process
+        """
+
+        if not self._is_deferred_to_bg(item):
+            super().finalize(settings, item)
+        else:
+            self.logger.info(
+                "Background publish enabled — deferring finalize to the background process."
+            )
+
+    def _is_deferred_to_bg(self, item) -> bool:
+        """Return True if publish item or any ancestor of it has bg publish enabled
+        but we are not currently in a bg process.
+        """
+        root = item
+        while not root.is_root:
+            root = root.parent
+        return bool(root.properties.get("bg_processing")) and not bool(
+            root.properties.get("in_bg_process")
+        )
 
 
 def _find_scene_animation_range():
